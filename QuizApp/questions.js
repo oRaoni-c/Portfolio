12de33322152940b4d0638398d4da7799274bf7a
oRaoni-c <ranierifrancoc@gmail.com>
2025-03-20 20:16:54 -0300

    Add files via upload

---
+++ QuizApp/questions.js
@@ -0,0 +1,61 @@
+let questions = [{
+        numb: 1,
+        question: "O que significa CSS?",
+        answer: "A. Cascading Style Sheets",
+        options: [
+            "A. Cascading Style Sheets",
+            "B. Colorful Style Sheets",
+            "C. Creative Style Sheets",
+            "D. Computer Style Sheets"
+        ],
+
+    },
+    {
+        numb: 2,
+        question: "Qual é o nome da tag que é ultilizada para usar o CSS dentro do HTML?",
+        answer: "C. Style.",
+        options: [
+            "A. Script.",
+            "B. Css.",
+            "C. Style.",
+            "D. Link.",
+        ],
+
+    },
+    {
+        numb: 3,
+        question: "Qual propriedade é usada para alterar a cor de fundo?",
+        answer: "D. Background-color",
+        options: [
+            "A. Background",
+            "B. Color",
+            "C. Bgcolor",
+            "D. Background-color"
+        ],
+
+    },
+    {
+        numb: 4,
+        question: "Qual propriedade CSS controla o tamanho do texto?",
+        answer: "B. Font-size",
+        options: [
+            "A. Text-size",
+            "B. Font-size",
+            "C. Font-style",
+            "D. Text-style"
+        ],
+
+    },
+    {
+        numb: 5,
+        question: "Como fazer com que cada palavra em um texto comece com letra maiúscula?",
+        answer: "C. Text-transform: capitalize",
+        options: [
+            "A. Text-transform: lowercase",
+            "B. Text-transform: uppercase",
+            "C. Text-transform: capitalize",
+            "D. Text-transform: none"
+        ],
+
+    }
+];
